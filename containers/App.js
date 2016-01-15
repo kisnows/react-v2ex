b968a55670822aa77116bfa756bd6c8eb4f9a082
kisnows <kisnows@users.noreply.github.com>
2016-01-15 16:59:15 +0800

    Update App.js

--- containers/App.js
+++ containers/App.js
@@ -1,6 +1,4 @@
 import React, { Component, PropTypes } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 
 import Header from '../components/Header'
 import Main from '../components/Main'
